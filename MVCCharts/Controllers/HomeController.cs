95e40d98dd6bbe624666a241ce1a924c900e7f9e
APTOL301938 <APTOL301938@APOLBO03-188>
2023-03-30 16:39:10 +0530

    Add project files.

---
+++ MVCCharts/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Runtime.Serialization;
+using MVCCharts.Models;
+using Newtonsoft.Json;
+
+namespace MVCCharts.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+
+            List<DataPoint> dataPoints = new List<DataPoint>{
+                new DataPoint(10, 22),
+                new DataPoint(20, 36),
+                new DataPoint(30, 42),
+                new DataPoint(40, 51),
+                new DataPoint(50, 46),
+            };
+
+            ViewBag.DataPoints = JsonConvert.SerializeObject(dataPoints);
+            return View();
+        }
+    }
+}
